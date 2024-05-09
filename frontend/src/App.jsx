16464a8f69612a0ca07caa0cac07c9a4b235901e
Farakh45 <farakh.riaz4455@gmail.com>
2024-05-09 10:42:16 +0200

    Fixing problems with data

---
+++ frontend/src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Home from './Pages/Home'
+
+import JokeList from './JokeList';
+
+function App() {
+  return (
+    <div>
+          <Home/>
+
+        <h1 style={{color: "rgb(30, 58, 138)",textAlign:"center"}}>Conditions Information</h1>
+      
+      <JokeList /> {/* Render the ConditionList component */}
+    </div>
+  );
+}
+
+export default App;
