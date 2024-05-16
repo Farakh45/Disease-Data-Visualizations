ec0c420ffc4bb6682b224bf4edf9a70f20f04661
Farakh45 <118170079+Farakh45@users.noreply.github.com>
2024-05-16 14:09:35 +0200

    error fixed

---
+++ frontend/src/Pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import PatientHome from '../components/PatientHome/PatientHome'
+
+
+const Home = () => {
+  return (
+    <div>
+      <PatientHome/>
+    </div>
+  )
+}
+
+export default Home
